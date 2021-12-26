34d48d1d55651d8c95838100b5c7cd061a8341ad
Altobert <alberto.sanmartin@usach.cl>
2021-12-26 19:17:44 -0300

    Se ordenan archivos

--- funciones.h
+++ funciones.h
@@ -1,11 +1,11 @@
 
 
-void *obtenerParteReal(void * params);
+void *obtenerParteReal(void *params);
 
-void *leerArchivoFloat(void * params);
+void *leerArchivoFloat(void *params);
 
-void * comparar(void* pair);
+void *comparar(void *params);
 
-void * estructuraResultados(void *val);
+void *estructuraResultados(void *params);
 
 void recibirArgumentos(int argc, char *argv[], int *h,int *c,int *u, int *n, int *flag);
